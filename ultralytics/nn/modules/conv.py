2adaca95dea9e65b9ac770f2dd34dd6ffded19ad
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-08-03 12:30:01 +0800

    try attetion mechanism v2 + spatial max pooling

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -79,16 +79,16 @@ class Conv_Spatial_Max_Pooling(nn.Module):
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
-        x = self.act(self.bn(self.conv(x)))
         x = self.sa(x)
         x = self.max_pool(x)
+        x = self.act(self.bn(self.conv(x)))
         return x
 
     def forward_fuse(self, x):
         """Perform transposed convolution of 2D data."""
-        x = self.act(self.conv(x))
         x = self.sa(x)
         x = self.max_pool(x)
+        x = self.act(self.conv(x))
         return x
 
 class Conv_Spatial_Max_Pooling_Dropout(nn.Module):
