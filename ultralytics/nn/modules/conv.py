6aa658b32260058b7c135442e0c36932ac69f36a
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-08-05 06:54:45 +0800

    spatial max pooling

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -74,21 +74,24 @@ class Conv_Spatial_Max_Pooling(nn.Module):
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
         self.max_pool = nn.MaxPool2d(3, stride=2)  # GAP layer
-        self.sa= SpatialAttention()
+        self.sa = SpatialAttention()
+        self.ca = ChannelAttention(c2)
         
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
+        x = self.act(self.bn(self.conv(x)))
         x = self.sa(x)
         x = self.max_pool(x)
-        x = self.act(self.bn(self.conv(x)))
+        x = self.ca(x)
         return x
 
     def forward_fuse(self, x):
         """Perform transposed convolution of 2D data."""
+        x = self.act(self.conv(x))
         x = self.sa(x)
         x = self.max_pool(x)
-        x = self.act(self.conv(x))
+        x = self.ca(x)
         return x
 
 class Conv_Spatial_Max_Pooling_Dropout(nn.Module):
