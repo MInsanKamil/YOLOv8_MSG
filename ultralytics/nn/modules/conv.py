21213657b1a8e9066178021485ec9a16ebe0f4a0
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-08-11 19:23:29 +0800

    ajdust kernel size attn pooling

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -199,7 +199,7 @@ class Conv_Attn(nn.Module):
 
     default_act = nn.SiLU()  # default activation
 
-    def __init__(self, c1, c2, k=1, s=1, km=3, p=None, g=1, d=1, act=True):
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
         """Initialize Conv layer with given arguments including activation."""
         super().__init__()
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
