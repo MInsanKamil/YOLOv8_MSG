f912ea35ab01db8075e20b331aa4cb0877409bfa
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-08-31 19:13:31 +0800

    Adjust Attn Mechanism

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -16,6 +16,7 @@ __all__ = (
     "HGStem",
     "SPP",
     "SPPF",
+    "SPPF_Attn",
     "C1",
     "C2",
     "C3",
@@ -185,6 +186,28 @@ class SPPF(nn.Module):
     def forward(self, x):
         """Forward pass through Ghost Convolution block."""
         y = [self.cv1(x)]
+        y.extend(self.m(y[-1]) for _ in range(3))
+        return self.cv2(torch.cat(y, 1))
+    
+class SPPF_Attn(nn.Module):
+    """Spatial Pyramid Pooling - Fast (SPPF) layer for YOLOv5 by Glenn Jocher."""
+
+    def __init__(self, c1, c2, k=5):
+        """
+        Initializes the SPPF layer with given input/output channels and kernel size.
+
+        This module is equivalent to SPP(k=(5, 9, 13)).
+        """
+        super().__init__()
+        c_ = c1 // 2  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_ * 4, c2, 1, 1)
+        self.cbam = CBAM(c_)
+        self.m = nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2)
+
+    def forward(self, x):
+        """Forward pass through Ghost Convolution block."""
+        y = [self.cbam(self.cv1(x))]
         y.extend(self.m(y[-1]) for _ in range(3))
         return self.cv2(torch.cat(y, 1))
 
