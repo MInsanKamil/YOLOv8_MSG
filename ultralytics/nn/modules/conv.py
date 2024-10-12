1b42257e66befd174d03f5fdfa4dd2d2abf7bc08
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-10-12 21:06:13 +0800

    ghost module modification

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -561,20 +561,19 @@ class GhostConv(nn.Module):
 class GhostConv_Modification(nn.Module):
     """Ghost Convolution https://github.com/huawei-noah/ghostnet."""
 
-    def __init__(self, c1, c2, k=1, s=1, g=1, act=True):
+    def __init__(self, c1, c2, k=1, s=1, k1=7, g=1, act=True):
         """Initializes the GhostConv object with input channels, output channels, kernel size, stride, groups and
         activation.
         """
         super().__init__()
         c_ = c2 // 2  # hidden channels
-        self.cv1 = Conv(c1, c_, k, s, None, g, act=act)
-        self.cv2 = Conv(c_, c_, 7, 1, None, c_, act=act)
-        self.cbam = CBAM(c_)
+        self.cv1 = Conv(c1, c_, k1, s, None, math.gcd(c1,c_), act=act)
+        self.cv2 = Conv(c_, c_, k, 1, None, g, act=act)
 
     def forward(self, x):
         """Forward propagation through a Ghost Bottleneck layer with skip connection."""
         y = self.cv1(x)
-        return torch.cat((y, self.cbam(self.cv2(y))), 1)
+        return torch.cat((y, self.cv2(y)), 1)
 
 
 class RepConv(nn.Module):
