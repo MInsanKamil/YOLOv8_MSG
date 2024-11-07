044059209b35a850f31d68c065cfa73eaf3fae72
MInsanKamil <89583296+MInsanKamil@users.noreply.github.com>
2024-11-07 22:31:13 +0800

    fix visualization plotting

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -1256,48 +1256,15 @@ def output_to_rotated_target(output, max_det=300):
     return targets[:, 0], targets[:, 1], targets[:, 2:-1], targets[:, -1]
 
 
-# def feature_visualization(x, module_type, stage, n=32, save_dir=Path("runs/detect/exp")):
-#     """
-#     Visualize feature maps of a given model module during inference.
-
-#     Args:
-#         x (torch.Tensor): Features to be visualized.
-#         module_type (str): Module type.
-#         stage (int): Module stage within the model.
-#         n (int, optional): Maximum number of feature maps to plot. Defaults to 32.
-#         save_dir (Path, optional): Directory to save results. Defaults to Path('runs/detect/exp').
-#     """
-#     for m in {"Detect", "Segment", "Pose", "Classify", "OBB", "RTDETRDecoder"}:  # all model heads
-#         if m in module_type:
-#             return
-#     if isinstance(x, torch.Tensor):
-#         _, channels, height, width = x.shape  # batch, channels, height, width
-#         if height > 1 and width > 1:
-#             f = save_dir / f"stage{stage}_{module_type.split('.')[-1]}_features.png"  # filename
-
-#             blocks = torch.chunk(x[0].cpu(), channels, dim=0)  # select batch index 0, block by channels
-#             # n = min(n, channels)  # number of plots
-#             n = min(n,16)
-#             _, ax = plt.subplots(math.ceil(n / 4), 4, tight_layout=True)  # 8 rows x n/8 cols
-#             ax = ax.ravel()
-#             plt.subplots_adjust(wspace=0.05, hspace=0.05)
-#             for i in range(n):
-#                 ax[i].imshow(blocks[i].squeeze())  # cmap='gray'
-#                 ax[i].axis("off")
-
-#             LOGGER.info(f"Saving {f}... ({n}/{channels})")
-#             plt.savefig(f, dpi=300, bbox_inches="tight")
-#             plt.close()
-#             np.save(str(f.with_suffix(".npy")), x[0].cpu().numpy())  # npy save
-
-def feature_visualization(x, module_type, stage, save_dir=Path("runs/detect/exp")):
+def feature_visualization(x, module_type, stage, n=16, save_dir=Path("runs/detect/exp")):
     """
-    Visualize a single feature map of a given model module during inference.
+    Visualize feature maps of a given model module during inference.
 
     Args:
         x (torch.Tensor): Features to be visualized.
         module_type (str): Module type.
         stage (int): Module stage within the model.
+        n (int, optional): Maximum number of feature maps to plot. Defaults to 32.
         save_dir (Path, optional): Directory to save results. Defaults to Path('runs/detect/exp').
     """
     for m in {"Detect", "Segment", "Pose", "Classify", "OBB", "RTDETRDecoder"}:  # all model heads
@@ -1306,14 +1273,47 @@ def feature_visualization(x, module_type, stage, save_dir=Path("runs/detect/exp"
     if isinstance(x, torch.Tensor):
         _, channels, height, width = x.shape  # batch, channels, height, width
         if height > 1 and width > 1:
-            f = save_dir / f"stage{stage}_{module_type.split('.')[-1]}_feature_single.png"  # filename
+            f = save_dir / f"stage{stage}_{module_type.split('.')[-1]}_features.png"  # filename
 
-            feature = x[0, 0].cpu().numpy()  # select batch index 0, channel 0
-            plt.imshow(feature, cmap='gray')
-            plt.axis("off")
+            blocks = torch.chunk(x[0].cpu(), channels, dim=0)  # select batch index 0, block by channels
+            # n = min(n, channels)  # number of plots
+            n = min(n,channels)
+            _, ax = plt.subplots(math.ceil(n / 4), 4, tight_layout=True)  # 4 rows x n/4 cols
+            ax = ax.ravel()
+            plt.subplots_adjust(wspace=0.05, hspace=0.05)
+            for i in range(n):
+                ax[i].imshow(blocks[i].squeeze())  # cmap='gray'
+                ax[i].axis("off")
 
-            LOGGER.info(f"Saving {f}... (1/{channels})")
+            LOGGER.info(f"Saving {f}... ({n}/{channels})")
             plt.savefig(f, dpi=300, bbox_inches="tight")
             plt.close()
             np.save(str(f.with_suffix(".npy")), x[0].cpu().numpy())  # npy save
+
+# def feature_visualization(x, module_type, stage, save_dir=Path("runs/detect/exp")):
+#     """
+#     Visualize a single feature map of a given model module during inference.
+
+#     Args:
+#         x (torch.Tensor): Features to be visualized.
+#         module_type (str): Module type.
+#         stage (int): Module stage within the model.
+#         save_dir (Path, optional): Directory to save results. Defaults to Path('runs/detect/exp').
+#     """
+#     for m in {"Detect", "Segment", "Pose", "Classify", "OBB", "RTDETRDecoder"}:  # all model heads
+#         if m in module_type:
+#             return
+#     if isinstance(x, torch.Tensor):
+#         _, channels, height, width = x.shape  # batch, channels, height, width
+#         if height > 1 and width > 1:
+#             f = save_dir / f"stage{stage}_{module_type.split('.')[-1]}_feature_single.png"  # filename
+
+#             feature = x[0, 0].cpu().numpy()  # select batch index 0, channel 0
+#             plt.imshow(feature, cmap='gray')
+#             plt.axis("off")
+
+#             LOGGER.info(f"Saving {f}... (1/{channels})")
+#             plt.savefig(f, dpi=300, bbox_inches="tight")
+#             plt.close()
+#             np.save(str(f.with_suffix(".npy")), x[0].cpu().numpy())  # npy save
 
